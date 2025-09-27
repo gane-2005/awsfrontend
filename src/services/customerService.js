f25dd2631d34c0a7b9edf41be292b47b6f486aef
ganesh <163662336+gane-2005@users.noreply.github.com>
2025-09-27 07:42:33 +0530

    Update customerService.js

--- src/services/customerService.js
+++ src/services/customerService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://ec2-34-228-168-121.compute-1.amazonaws.com:8081/api/customers';
+const API_URL = 'http://ec2-18-205-240-234.compute-1.amazonaws.com:8081/api/customers';
 
 export const getCustomers = async () => {
   const response = await axios.get(API_URL);
